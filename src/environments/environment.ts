2b406cfa3981b342bf8194f06191de069bc46c82
Ranjeet Singh <37861431+ranjeetlove@users.noreply.github.com>
2024-03-18 00:33:54 +0530

    Delete src/environments/environment.ts

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-// Your web app's Firebase configuration
-export const firebaseConfig = {
-    apiKey: "AIzaSyA3T4WwrKEocpvC6pWCtZpBqSqXZVQsW3E",
-    authDomain: "user-management-bd03b.firebaseapp.com",
-    projectId: "user-management-bd03b",
-    storageBucket: "user-management-bd03b.appspot.com",
-    messagingSenderId: "644597948192",
-    appId: "1:644597948192:web:832c364af07deda14c8145"
-  };
